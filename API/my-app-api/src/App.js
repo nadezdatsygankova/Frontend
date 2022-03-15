15f05815e21d1ddf7fef0dd0a4040cf72f8356c9
nadezdatsygankova <nadiapritchina@gamil.com>
2022-03-14 23:32:40 -0400

    add average

--- API/my-app-api/src/App.js
+++ API/my-app-api/src/App.js
@@ -1,29 +1,67 @@
 
 import './App.css';
-//useState hook
-import { useState } from 'react';
-import axios from 'axios';
+import Posts from './Component/Posts';
 
-function App() {
-  const [data, setData] = useState();
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-  const apiURL = ' https://api.hatchways.io/assessment/students';
+export default function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          ` https://api.hatchways.io/assessment/students`
+        );
+
+        // console.log(response.data);
+        let responseDataArray = response.data.students;
+        console.log(responseDataArray);
+        setData(responseDataArray);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
 
 
-  const fetchData = async () => {
-    const response = await axios.get(apiURL);
 
-    setData(response.data);
-  }
+
+
+
+
   return (
     <div className="App">
-      <h1>Data</h1>
-      {/* Fetch data from an API */}
-      <div>
-        <button className='fetch-button' onClick={fetchData}
-      </div>
+      <h1>API Posts</h1>
+      {loading && <div>A moment please...</div>}
+      {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+
+      {data &&
+        data.map(({ id, lastName, email, company, skill, grades }) => (
+          <div key={id}>
+            <h3> {lastName}</h3>
+            <p>Email: {email}</p>
+            <p>Company: {company}</p>
+            <p>Skill: {skill}</p>
+            <Posts average={grades} />
+            {/* <p>average({grades})</p> */}
+          </div>
+        ))}
+
     </div>
   );
 }
 
-export default App;
+// arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let average = (array) => array.reduce((a, b) => a + b) / array.length;
+// console.log(average(arr));
---
+++ API/my-app-api/src/Component/Posts.js
@@ -0,0 +1,13 @@
+
+export default function Posts(props) {
+
+    let propsAverage = props.average.map(Number);
+    let average = (propsAverage) => propsAverage.reduce((a, b) => a + b) / propsAverage.length;
+    let result = average(propsAverage);
+
+    return (
+        <div>
+            <p>Average: {result} % </p>
+        </div>
+    )
+}
